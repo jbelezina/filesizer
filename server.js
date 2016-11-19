d4338d9b3fa62141d1697c2675a99dc3878f661f
Jan Belezina <jbelezina@gmail.com>
2016-11-19 18:29:07 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+let express = require('express'),
+    multer  = require('multer'),
+    upload = multer({ dest: 'upload/' }),
+    fs = require('fs'),
+    portNo = 8080 || process.env.PORT;
+    
+let app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    res.render('home');
+})
+
+app.post('/upload', upload.single('uploadedfile'), function (req, res, next) {
+  
+    let file = req.file;
+    let size = file.size;
+    
+     res.json({
+        'size': size
+    })
+});
+
+app.listen(8080, ()=>{console.log('server runnin on port' + portNo)});
